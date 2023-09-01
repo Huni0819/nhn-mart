f82ecc8d526c3ac2582c6c7e33d63c84c955e35b
{Huni0819} <{orutaku15@gmail.com}>
2023-09-01 16:32:42 +0900

    Logback 추가 및 FoodStand add 메서드 구현

--- src/main/java/com/nhnacademy/mart/Food.java
+++ src/main/java/com/nhnacademy/mart/Food.java
@@ -1,11 +1,22 @@
 package com.nhnacademy.mart;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Food {
+    private static final Logger logger = LoggerFactory.getLogger(Food.class);
 
     private final String name;
     private final int price;
 
-
+    public Food(String name, int price) {
+        if(price <= 0) {
+            logger.warn("상품의 가격은 0원 이하가 될 수 없습니다.");
+            throw new IllegalArgumentException("상품의 가격은 0원 이하가 될 수 없습니다.");
+        }
+        this.name = name;
+        this.price = price;
+    }
 
     public String getName() {
         return name;
