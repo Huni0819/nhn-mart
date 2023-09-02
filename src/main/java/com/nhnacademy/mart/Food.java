f4fecec4fd4d8d65832db2b6960534f06ce3bbab
{Huni0819} <{orutaku15@gmail.com}>
2023-09-02 13:40:59 +0900

    StringUtil 사용을 위한 의존성 추가 및 Food constructor 예외 추가

--- src/main/java/com/nhnacademy/mart/Food.java
+++ src/main/java/com/nhnacademy/mart/Food.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.mart;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +11,10 @@ public class Food {
     private final int price;
 
     public Food(String name, int price) {
+        if(StringUtils.isEmpty(name)) {
+            logger.warn("상품의 이름은 공백이거나 Null값이 들어올 수 없습니다.");
+            throw new IllegalArgumentException("상품의 이름은 공백이거나 Null값이 들어올 수 없습니다.");
+        }
         if(price <= 0) {
             logger.warn("상품의 가격은 0원 이하가 될 수 없습니다.");
             throw new IllegalArgumentException("상품의 가격은 0원 이하가 될 수 없습니다.");
